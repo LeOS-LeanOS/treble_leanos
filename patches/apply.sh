8f836d7d4c7f03d3aedd668e3e33ebacced88d72
Christopher A. Williamson <contact@chrisaw.io>
2025-08-03 19:11:08 +0100

    Add patch apply script, move possible junk to temp-stash

---
+++ patches/apply.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+patches=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+tree="$2"
+
+echo "Applying ${tree} patches:"
+
+for project in $(cd "$patches"/"$tree"; echo *); do
+    echo "> ${project}"
+    p="$(tr _ / <<<"$project" |sed -e 's;platform/;;g')"
+    [ "$p" == build ] && p=build/make
+    [ "$p" == treble/app ] && p=treble_app
+    [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
+    [ "$p" == vendor/partner/gms ] && p=vendor/partner_gms
+    pushd "$p" &>/dev/null
+    for patch in "$patches"/"$tree"/"$project"/*.patch; do
+        echo ">> ${patch}"
+        git am "$patch" || exit
+    done
+    popd &>/dev/null
+done
