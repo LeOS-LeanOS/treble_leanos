098d06b150ef1721e82f03fbd36c6d3f7440a87d
Christopher A. Williamson <contact@chrisaw.io>
2025-08-05 17:06:17 +0100

    Simplify APK names

--- vendor/LeOS/prebuiltapks/LeOS-Droid/Android.mk
+++ vendor/LeOS/prebuiltapks/LeOS-Droid/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := LeOS-Droid
-LOCAL_SRC_FILES := LeOS-Droid-v1.6.4.apk
+LOCAL_SRC_FILES := LeOS-Droid.apk
 LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := PRESIGNED
