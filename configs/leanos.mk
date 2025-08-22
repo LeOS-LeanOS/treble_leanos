9fa91bbe8a5b5362334e019e718b6dcbb7ffebac
Christopher A. Williamson <contact@chrisaw.io>
2025-08-22 17:10:26 +0100

    Add face unlock support (thanks to ponces)

--- configs/leanos.mk
+++ configs/leanos.mk
@@ -1,2 +1,8 @@
 # ROM Configuration for LeanOS builds
 # This file is copied to device/phh/treble/leanos.mk during the build process
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:system/etc/permissions/android.hardware.biometrics.face.xml
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
